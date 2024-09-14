6c4bee1a2fab5dfe2f8bdac9378e786a300537a5
Bernardita1010 <be.riquelmep@duocuc.cl>
2024-09-14 08:15:21 -0300

    orden de código

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,24 +12,19 @@ export class HomeComponent implements OnInit {
   loginUsers: any[] = [];
   currentUser: any = null;
   userName: string = '';
-
   constructor( private router: Router) { }
-
   ngOnInit(): void {
     // Recuperar el arreglo de signupUsers desde el localStorage
     const signupData = localStorage.getItem('signupUsers');
     if (signupData) {
       this.signupUsers = JSON.parse(signupData);
     }
-
     // Recuperar el arreglo de loginUsers desde el localStorage
     const loginData = localStorage.getItem('loginUsers');
     if (loginData) {
       this.loginUsers = JSON.parse(loginData);
-
       // Obtener el último usuario logueado (el último en el arreglo)
       this.currentUser = this.loginUsers[this.loginUsers.length - 1];
-      
       // Buscar el userName correspondiente al email del usuario logueado
       const matchedUser = this.signupUsers.find(user => user.email === this.currentUser.email);
       if (matchedUser) {
@@ -41,9 +35,7 @@ export class HomeComponent implements OnInit {
   navigateTo(route: string): void {
     this.router.navigate([route]); // Navegar a la ruta especificada
   }
-
   logout(): void {
-
     this.router.navigate(['/splash']);  // Redirigir al login al cerrar sesión
   }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,69 +8,36 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   signupUsers: any[] = [];
   signupObj: any = {
-    userName: '',
-    email: '',
-    password: '',
-    passwordV: ''
-  };
+    userName: '',email: '',password: '',passwordV: ''};
   loginUsers: any[] = [];
-  loginObj: any = {
-    email: '',
-    password: ''
-  };
-
+  loginObj: any = {email: '',password: ''};
   constructor(private router: Router) {}
-
   ngOnInit(): void {
     const localData = localStorage.getItem('signupUsers');
     if (localData != null) {
-      this.signupUsers = JSON.parse(localData);
-    }
-
+      this.signupUsers = JSON.parse(localData);}
     const loginData = localStorage.getItem('loginUsers');
     if (loginData != null) {
-      this.loginUsers = JSON.parse(loginData);
-    }
-  }
-
+      this.loginUsers = JSON.parse(loginData);}}
   onSignUp(): void {
     if (this.signupObj.password === this.signupObj.passwordV && this.signupObj.password != '') {
       this.signupUsers.push({ ...this.signupObj }); // Guardar una copia del objeto actual
       localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
       alert('Registro exitoso');
-
       // Limpiar el formulario
-      this.signupObj = {
-        userName: '',
-        email: '',
-        password: '',
-        passwordV: ''
-      };
-      window.location.reload();
-    } else {
-      alert('El campo está vacío o Las contraseñas no coinciden.');
-    }
-  }
+      this.signupObj = {userName: '',email: '',password: '',passwordV: ''};
+      window.location.reload();} else {
+      alert('El campo está vacío o Las contraseñas no coinciden.');}}
   reestablecer (){
-    this.router.navigate(['/reestablecer']);
-  }
+    this.router.navigate(['/reestablecer']);}
   onLogIn(): void {
     if (this.loginObj.email !== '' && this.loginObj.password !== '') {
       const isUserExist = this.signupUsers.find(
-        m => m.email === this.loginObj.email && m.password === this.loginObj.password
-      );
+        m => m.email === this.loginObj.email && m.password === this.loginObj.password);
       if (isUserExist) {
         alert('Usuario ingresado con éxito');
         this.loginUsers.push({ ...this.loginObj }); // Guardar una copia del objeto actual
         localStorage.setItem('loginUsers', JSON.stringify(this.loginUsers));
         this.router.navigate(['/home']);
-      } else {
-        alert('Credenciales incorrectas');
-      }
-    } else {
-      alert('Favor ingresar credenciales');
-    }
-    
-  }
-
-}
+      } else { alert('Credenciales incorrectas'); }
+    } else { alert('Favor ingresar credenciales');}}}

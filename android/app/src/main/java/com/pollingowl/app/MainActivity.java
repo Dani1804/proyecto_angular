26b07a2ce3cad2bbdd9a85f8c35b866b074d12d6
Dani1804 <da.lizama@duocuc.cl>
2024-12-14 08:51:47 -0300

    apk generada

---
+++ android/app/src/main/java/com/pollingowl/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pollingowl.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pollingowl.app',
+  appName: 'ProyectoProgramacionMobile',
+  webDir: 'dist/ProyectoProgramacionMobile/browser'
+};
+
+export default config;
